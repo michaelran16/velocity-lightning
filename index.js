273a88e2db604abf8a21a02fe24909532bccbf97
Michael <michaelran16@gmail.com>
2019-03-28 20:02:40 +0800

    add logging to server

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = new Koa();
 const views = require('koa-views')
 const path = require('path')
 
-
 app.use(views(path.join(__dirname, './views'), {
   extension: 'ejs'
 }))
 app.use(require('./routers/stellar.js').routes())
 
 app.listen(3000);
+console.log(`listening on port 3000`)
