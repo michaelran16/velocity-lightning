af9d9252116b195a2d6d9caddfa67d5e6f71dcd9
Michael <michaelran16@gmail.com>
2019-03-28 20:00:10 +0800

    add views to koa

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const Koa = require('koa');
 const app = new Koa();
+const views = require('koa-views')
+const path = require('path')
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
-});
 
+app.use(views(path.join(__dirname, './views'), {
+  extension: 'ejs'
+}))
 app.use(require('./routers/stellar.js').routes())
+
 app.listen(3000);
