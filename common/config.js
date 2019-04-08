239cc109ad277ed4d0715925e28a46bbdd6f9d16
Michael <michaelran16@gmail.com>
2019-04-08 10:58:30 +0800

    Init Koa

---
+++ common/config.js
@@ -0,0 +1,12 @@
+const config = {
+	database : {
+		DATABASE : 'lightning-test-stellar',
+		USERNAME : 'root',
+		PASSWORD : 'root',
+		PORT : 3306,
+		HOST : '127.0.0.1',
+	},
+	// MySQL port
+	port : 7000,
+}
+module.exports = config
--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const koa = require('koa')
 const path = require('path')
+const bodyParser = require('koa-bodyparser')
 const ejs = require('ejs')
 const router = require('koa-router')
 const views = require('koa-views')
+const koaStatic = require('koa-static')
+const staticCache = require('koa-static-cache')
+const socket = require('socket.io')
 
 const app = new koa()
+app.keys = ['secret'];
+
+app.use(koaStatic(
+	path.join(__dirname, './static')
+))
+
 app.use(views(path.join(__dirname, './views'), {
   extension: 'ejs'
+}))
+app.use(bodyParser({
+	formLimit : '1mb'
 }))
 
 // routing config
